441e175475de58b895095bc8faddc26305700aea
suryaprakashselvamani <suryaprakashs.cloud@gmail.com>
2025-07-12 18:13:18 +0530

    modified

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/my-fastapi-app
-nohup uvicorn app.main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
+nohup python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
